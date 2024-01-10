44cd423ccebaa5e79cbf75b49d94528bd868157b
Jake S <jsweg13@gmail.com>
2024-01-10 15:53:37 -0500

    Create class Tpoic and add text and date

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
 from django import models
 
-# Create models
+class Topic(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
